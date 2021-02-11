362feb01cab5ea1cddd33444c7cebc526eaa3c13
test-open <76162523+test-open@users.noreply.github.com>
2021-02-12 02:06:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,22 +21,28 @@
 #rm -rf feeds/small/shadowsocksr-libev
 #rm -rf package/feeds/kenzo/luci-app-vssr
 #rm -rf package/feeds/small/shadowsocksr-libev
-#git clone https://github.com/immortalwrt/openwrt-passwall package/diy/openwrt_passwall 
+
 #passwall
+#git clone https://github.com/immortalwrt/openwrt-passwall package/diy/openwrt_passwall 
 #rm -rf package/diy/openwrt_passwall/luci-app-passwall
 #svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-passwall package/diy/openwrt_passwall/luci-app-passwall
 #mv package/diy/openwrt_passwall/luci-app-passwall/po/zh-cn package/diy/openwrt_passwall/luci-app-passwall/po/zh_Hans
 #sed -i 's/PassWall/科学上网/g' package/diy/openwrt_passwall/luci-app-passwall/po/zh_Hans/passwall.po
 #sed -i 's/zh-cn/zh_Hans/g' package/diy/openwrt_passwall/luci-app-passwall/Makefile
+
 #主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/diy/luci-theme-argon
+
 #DDNS
 svn co https://github.com/garypang13/openwrt-packages/trunk/ddns-scripts-aliyun package/diy/ddns-scripts-aliyun
 svn co https://github.com/garypang13/openwrt-packages/trunk/ddns-scripts-dnspod package/diy/ddns-scripts-dnspod
+
 #清内存
 svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-ramfree package/diy/luci-app-ramfree
+
 #定时重启
 svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-autoreboot package/diy/luci-app-autoreboot
+
 #zerotier
 svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-zerotier package/diy/luci-app-zerotier
 sed -i 's/msgid ""/msgid "VPN"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
@@ -46,6 +52,7 @@ sed -i 's/msgstr "接口信息"/msgstr "基础设置"/g' package/diy/luci-app-ze
 sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 sed -i '$a msgid "Interface Info"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 sed -i '$a msgstr "接口信息"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+
 #mt-drivers
 svn co https://github.com/garypang13/openwrt-packages/trunk/mt-drivers package/diy/mt-drivers
 
