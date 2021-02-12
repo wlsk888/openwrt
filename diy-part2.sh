60f8ef47f2dc025994ea7698b2ba150187e778ef
test-open <76162523+test-open@users.noreply.github.com>
2021-02-12 15:23:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,11 +13,11 @@
 # Modify default IP
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
-sed -i '$a msgid "VPN"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i '$a msgstr "虚拟网络"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+sed -i 's/msgid "Interface Info"/msgid "VPN"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+sed -i 's/msgstr "接口信息"/msgstr "虚拟网络"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i 's/msgid "Interface Info"/msgid "Base Setting"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
-sed -i 's/msgstr "接口信息"/msgstr "基本设置"/g' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+sed -i '$a msgid "Base Setting"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
+sed -i '$a msgstr "基本设置"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 sed -i '$a \\' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 sed -i '$a msgid "Interface Info"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
 sed -i '$a msgstr "接口信息"' package/diy/luci-app-zerotier/po/zh_Hans/zerotier.po
