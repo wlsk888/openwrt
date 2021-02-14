5dd81e7dfba03b1c18eacf87b70634e8bd55c23a
test-open <76162523+test-open@users.noreply.github.com>
2021-02-15 00:03:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,3 +28,5 @@ ln -s feeds/kenzo/luci-app-passwall/po/zh-cn feeds/kenzo/luci-app-passwall/po/zh
 #ByPass依赖
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+#测试
+svn co https://github.com/garypang13/openwrt-packages/trunk/default-settings package/diy/default-settings
